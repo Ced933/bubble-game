f6f524d75ee6c3d1ee6ba2e4a89dfc4be29881d7
Cedric Glou <cedricglou@MacBook-Air-de-Cedric.local>
2024-01-31 16:24:36 +0100

    bubble game fini

--- index.ts
+++ index.ts
@@ -1,34 +1,38 @@
+let counterDisplay:any = document.querySelector('h3')
+let counter:number = 0;
+
+
 const bubbleMaker = ()=>{
 
     const bubble = document.createElement('span');
     bubble.classList.add('bubble');
     document.body.appendChild(bubble);
-    // taille compris de la bulle entre 100 et 300
+    // Taille compris de la bulle entre 100 et 300
     const size:number | string = Math.random() * 200 + 100 + "px";
-    console.log(size);
+    // On veut que la bulle soit un carré et on lui rajoutera l'arrondi en css size est dynamique car on veut des bulles de toutes tailles
     bubble.style.height= size;
     bubble.style.width= size;
-    
+    // La position de bulle dans le body en top et en left 
     bubble.style.top = Math.random()*100 + 50 +"%";
     bubble.style.left = Math.random()*100+"%";
+    // plusMinus va servir à avoir des chiffres positif et negatif pour fluctuer la direction des ballons à gauche ou à droite 
     const plusMinus = Math.random() > 0.5 ? 1 : -1
     bubble.style.setProperty('--left', Math.random()*100 * plusMinus +"%")
 
     setTimeout(()=>{
+        // au bout de 8s(durée de l'animation ds le css) tu fais disparaître le balon créé 
         bubble.remove()
     },8000)
+
+    bubble.addEventListener('click',()=>{
+        // à chaque ballon cliqué tu le fais disparaître et tu rajoute +1 dans le counter  
+        counter++;
+        counterDisplay.textContent = counter
+        bubble.remove();
+    })
 };
 
 
 
 setInterval(bubbleMaker,3000);
 
-document.body.addEventListener('click', (e:any)=>{
-    let counter:number = 0;
-let p:any = document.createElement('p');
-   e.target.remove();
-   counter++;
-   p=counter;
-   document.body.appendChild(p)
-   console.log(counter)
-})
